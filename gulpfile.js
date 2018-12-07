31b8cea19fbacd8961bfb5dacfc7d24dc0df06e1
oti <otiext@gmail.com>
2018-12-08 01:32:25 +0900

    watchが動かないやんけ！！

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const
   gulp = require('gulp')
 
@@ -17,13 +19,15 @@ const
 , misc = require('./task/asset/misc')
 
 // 記事オブジェクト作成タスク ====================
+// （post/*.md -> posts.json -> archives.json, tags.json, years.json）
+// （demo/*.md -> demos.json -> demo-archives.json）
 , json_posts = require('./task/json/posts')
 , json_demos = require('./task/json/demos')
 
 
 // HTML作成タスク ====================
 
-// 記事個別ページ作成（post/*.md -> posts.json -> archives.json, tags.json, years.json）
+// 記事個別ページ作成（post_id.md -> post_id.html）
 , html_posts = require('./task/html/posts')
 
 // 記事一覧ページ作成（archives_name.md -> archives_name.html）
@@ -43,15 +47,11 @@ const
 
 // watch
 , watch = done => {
-  const options = {
-    delay: 50,
-  }
-
-  gulp.watch('./src/style/**/*', options, css)
-  gulp.watch('./src/image/**/*', options, image)
-  gulp.watch('./src/misc/**/*',  options, misc)
-  gulp.watch('./src/html/**/*',  options, gulp.task('html'))
-  gulp.watch('./src/md/**/*',    options, gulp.task('md'))
+  gulp.watch(['./src/style/**/*'], css)
+  gulp.watch(['./src/image/**/*'], image)
+  gulp.watch(['./src/misc/**/*'],  misc)
+  gulp.watch(['./src/html/**/*'],  gulp.task('html'))
+  gulp.watch(['./src/md/**/*'],    gulp.task('md'))
   done()
 }
 
