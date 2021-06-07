a76b38d8322966726c45bf25a5d30d7dce84b2cb
oti <otiext@gmail.com>
2021-06-08 01:06:16 +0900

    fix(task): remove `csswring()`

--- task/asset/css.js
+++ task/asset/css.js
@@ -3,14 +3,13 @@ const browserSync = require("browser-sync");
 const plumber = require("gulp-plumber");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-const csswring = require("csswring");
 const mqpacker = require("css-mqpacker");
 
 const css = () => {
   return gulp
     .src("./src/style/*.css")
     .pipe(plumber())
-    .pipe(postcss([autoprefixer({ grid: true }), mqpacker(), csswring()]))
+    .pipe(postcss([autoprefixer({ grid: true }), mqpacker()]))
     .pipe(gulp.dest("./dist/css/"))
     .pipe(browserSync.stream());
 };
