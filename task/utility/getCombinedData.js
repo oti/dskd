461a08375ce657c21ef7acd01dc20c99cc575e83
oti <otiext@gmail.com>
2022-02-07 15:56:22 +0900

    fix(task): utility/getCombinedData.js, fix `__dirname` define

--- task/utility/getCombinedData.js
+++ task/utility/getCombinedData.js
@@ -1,5 +1,9 @@
 import fs from "fs";
 import path from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const getCombinedData = (frontMatter) => {
   const _config = fs.readFileSync(
