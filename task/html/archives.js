3169fdbec415a79c0b74051b7fdb0fc89857ff7d
oti <otiext@gmail.com>
2022-02-07 12:27:22 +0900

    feat(task): html/archives.js, change to using ESModule

--- task/html/archives.js
+++ task/html/archives.js
@@ -1,30 +1,18 @@
-const package = require("../../package.json");
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const frontMatter = require("gulp-front-matter");
-const prettify = require("gulp-prettify");
-const layout = require("gulp-layout");
-const md = require("gulp-markdown");
-const config = require("../../blogconfig.json");
-
-config.blog_version = package.version;
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import frontMatter from "gulp-front-matter";
+import prettify from "gulp-prettify";
+import layout from "gulp-layout";
+import md from "gulp-markdown";
+import { getCombinedData } from "../utility/getCombinedData";
 
 // 記事一覧ページ作成（archives_name.md -> archives_name.html）
-const archives = () => {
-  return gulp
+export const archives = () =>
+  gulp
     .src("./src/md/archives/*.md")
     .pipe(plumber())
     .pipe(frontMatter())
     .pipe(md())
-    .pipe(
-      layout((file) => ({
-        ...config,
-        ...file.frontMatter,
-        ...require("../../src/json/posts.json"),
-      }))
-    )
+    .pipe(layout(({ frontMatter }) => getCombinedData(frontMatter)))
     .pipe(prettify({ indent_char: " ", indent_size: 2 }))
     .pipe(gulp.dest("./dist/archives/"));
-};
-
-module.exports = archives;
