9ddd35e8839aed9f6812ce6221a9f295cea86272
oti <otiext@gmail.com>
2022-02-07 12:28:18 +0900

    feat(task): html/posts.js, change to using ESModule

--- task/html/posts.js
+++ task/html/posts.js
@@ -1,30 +1,18 @@
-const package = require("../../package.json");
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const frontMatter = require("gulp-front-matter");
-const prettify = require("gulp-prettify");
-const layout = require("gulp-layout");
-const md = require("gulp-markdown");
-const config = require("../../blogconfig.json");
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import frontMatter from "gulp-front-matter";
+import prettify from "gulp-prettify";
+import layout from "gulp-layout";
+import md from "gulp-markdown";
+import { getCombinedData } from "../utility/getCombinedData";
 
-config.blog_version = package.version;
-
-// 全記事作成（post_id.md -> post_id.html）
-const posts = () => {
-  return gulp
-    .src("./src/md/post/*.md")
+// ブログインデックス作成（index.md -> index.html）
+export const posts = () =>
+  gulp
+    .src("./src/md/post/*.md", { allowEmpty: true })
     .pipe(plumber())
     .pipe(frontMatter())
     .pipe(md())
-    .pipe(
-      layout((file) => ({
-        ...config,
-        ...file.frontMatter,
-        ...require("../../src/json/posts.json"),
-      }))
-    )
+    .pipe(layout(({ frontMatter }) => getCombinedData(frontMatter)))
     .pipe(prettify({ indent_char: " ", indent_size: 2 }))
     .pipe(gulp.dest("./dist/archives/"));
-};
-
-module.exports = posts;
