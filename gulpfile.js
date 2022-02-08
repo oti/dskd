227ba5d4f41f86b112882da803fddb7ab52759ce
oti <otiext@gmail.com>
2022-02-08 21:34:30 +0900

    refactor(task): gulpfile.js, add `asset` task and refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -20,30 +20,26 @@ export const pages = () =>
 export const posts = () =>
   html({ src: "./src/md/post/*.md", dest: "./dist/archives/" });
 
+// アセット更新
+export const asset = gulp.parallel(favicon, image, css, misc);
+
 // テンプレート更新
 export const template = gulp.parallel(archives, feed, pages, posts);
 
+// 記事更新
+export const md = gulp.series(data, html);
+
+// ビルド
+export const build = gulp.series(asset, data, template);
+
 // ファイル監視
 export const observe = (done) => {
-  gulp.watch(["./src/image/**/*"], image);
   gulp.watch(["./src/html/**/*"], template);
+  gulp.watch(["./src/image/**/*"], image);
   gulp.watch(["./src/misc/**/*"], misc);
   gulp.watch(["./src/style/**/*"], css);
   done();
 };
-
-// ビルド
-export const build = gulp.series(
-  gulp.parallel(favicon, image, css, misc),
-  data,
-  gulp.parallel(archives, feed, pages, posts)
-);
-
-// 記事更新
-export const md = gulp.series(
-  data,
-  gulp.parallel(archives, feed, pages, posts)
-);
 
 // デフォルトタスク
 export default gulp.series(build, server, observe);
