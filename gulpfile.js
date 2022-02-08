cfbcab6191c0da334ac5604c4463bd327357b7c3
oti <otiext@gmail.com>
2022-02-08 21:30:21 +0900

    chore: gulpfile.js, remove unnecessary comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,17 @@
 import gulp from "gulp";
 
-// server & browser sync
 import { server } from "./task/server/server.js";
 
-// アセット作成タスク
 import { css } from "./task/asset/css.js";
 import { favicon } from "./task/asset/favicon.js";
 import { image } from "./task/asset/image.js";
 import { misc } from "./task/asset/misc.js";
 
-// 記事オブジェクト作成（post/*.md -> posts.json）
 import { data } from "./task/json/data.js";
 
-// 記事一覧ページ作成（archives_name.md -> archives_name.html）
 import { archives } from "./task/html/archives.js";
-// RSS作成（feed.md -> feed）
 import { feed } from "./task/html/feed.js";
-// ブログインデックス作成（index.md -> index.html）
 import { pages } from "./task/html/pages.js";
-// 記事個別ページ作成（post_id.md -> post_id.html）
 import { posts } from "./task/html/posts.js";
 
 // ファイル監視
