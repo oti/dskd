8fadda10fc460e77d813f5bb3f8c954eb7b85428
oti <otiext@gmail.com>
2022-02-08 21:29:04 +0900

    feat(task): html/html.js

---
+++ task/html/html.js
@@ -0,0 +1,20 @@
+import gulp from "gulp";
+import frontMatter from "gulp-front-matter";
+import gulpIf from "gulp-if";
+import layout from "gulp-layout";
+import md from "gulp-markdown";
+import plumber from "gulp-plumber";
+import prettify from "gulp-prettify";
+import rename from "gulp-rename";
+import { getCombinedData } from "../utility/getCombinedData.js";
+
+export const html = ({ src, dest, extname }) =>
+  gulp
+    .src(src, { allowEmpty: true })
+    .pipe(plumber())
+    .pipe(frontMatter())
+    .pipe(md())
+    .pipe(layout(({ frontMatter }) => getCombinedData(frontMatter)))
+    .pipe(prettify({ indent_char: " ", indent_size: 2 }))
+    .pipe(gulpIf(extname, rename({ extname: ".xml" })))
+    .pipe(gulp.dest(dest));
