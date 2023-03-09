51cc86765a3ad9bcbc9a6de45b657878e6e03386
oti <otiext@gmail.com>
2023-03-10 08:23:08 +0900

    wip: task/post/post.mjs, fast-glob の代わりに fs/promise.readdir を使うようにした

--- task/post/post.mjs
+++ task/post/post.mjs
@@ -6,12 +6,17 @@ import pug from "pug";
 import md2Pug from "markdown-to-pug";
 const m2p = new md2Pug();
 
+const postMdPath = "src/md/post/";
+const postPugPath = "src/html/archives/";
+
 const getPostMatter = async () => {
   return Promise.all(
-    (await fg("src/md/post/*.md")).map(async (filepath) => {
-      const content = await fs.readFile(filepath, "utf8");
-      return matter(content);
-    })
+    (await fs.readdir(postMdPath, { withFileTypes: true }))
+      .filter((dirent) => dirent.isFile())
+      .map(async ({ name }) => {
+        const content = await fs.readFile(`${postMdPath}${name}`, "utf8");
+        return matter(content);
+      })
   );
 };
 
@@ -24,21 +29,23 @@ const getPostPug = async (matters) => {
         .map((v) => `  ${v}`)
         .join("\n");
       const formatted = `extends ${data.layout}\n\nblock contents\n${indentedPug}`;
-      fs.mkdir("src/html/pug/", { recursive: true });
-      await fs.writeFile(`src/html/pug/${data.page_id}.pug`, formatted);
+      fs.mkdir("src/html/archives/", { recursive: true });
+      await fs.writeFile(`src/html/archives/${data.page_id}.pug`, formatted);
     })
   );
 };
 
 const getPostHtml = async () => {
   return Promise.all(
-    (await fg("src/html/pug/*.pug")).map(async (filepath) => {
-      const filename = filepath.split(".pug")[0].at(-1);
-      const content = await fs.readFile(filepath, "utf8");
-      const compiled = pug.render(content, { filename });
-      fs.mkdir("dist/archives/", { recursive: true });
-      await fs.writeFile(`dist/archives/${filename}.html`, compiled);
-    })
+    (await fs.readdir(postPugPath, { withFileTypes: true }))
+      .filter((dirent) => dirent.isFile())
+      .map(async ({ name }) => {
+        const filename = name.split(".pug")[0].at(-1);
+        const content = await fs.readFile(`${postPugPath}${name}`, "utf8");
+        const compiled = pug.render(content, { filename });
+        fs.mkdir("dist/archives/", { recursive: true });
+        await fs.writeFile(`dist/archives/${filename}.html`, compiled);
+      })
   );
 };
 
