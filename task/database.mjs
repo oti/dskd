6c0227bf6a4cebff750e0d74e49be3758ecaaa51
oti <otiext@gmail.com>
2023-03-15 19:17:29 +0900

    refactor: task/database.mjs

--- task/database.mjs
+++ task/database.mjs
@@ -2,16 +2,16 @@ import pkg from "../package.json" assert { type: "json" };
 
 export const database = async (matters) => {
   const posts = matters
-    .filter(({ data: { type } }) => type === "post")
+    .filter((item) => item.data.type === "post")
     // 記事は日付で降順ソートする
     .sort(
       (a, b) =>
         Number(b.data.datetime.replace(/[-T:]/g, "")) -
         Number(a.data.datetime.replace(/[-T:]/g, ""))
     )
-    .map((post, i, sortedPosts) => {
-      const older = sortedPosts[i + 1];
-      const newer = sortedPosts[i - 1];
+    .map((post, i, sorted) => {
+      const older = sorted[i + 1];
+      const newer = sorted[i - 1];
       return {
         content: post.content,
         ...post.data,
@@ -31,7 +31,7 @@ export const database = async (matters) => {
     });
 
   const pages = matters
-    .filter(({ data: { type } }) => type === "page")
+    .filter((item) => item.data.type === "page")
     .map((page) => {
       return {
         content: page.content,
@@ -43,7 +43,7 @@ export const database = async (matters) => {
     .flatMap((post) => post.tag.map((tag) => ({ [tag]: post })))
     .reduce((memo, pair) => {
       Object.keys(pair).map((key) => {
-        if (!memo[key]) memo[key] = [];
+        memo[key] = memo[key] ? memo[key] : [];
         memo[key].push(pair[key]);
       });
       return memo;
@@ -51,7 +51,7 @@ export const database = async (matters) => {
 
   const years = posts.reduce((memo, post) => {
     const year = post.datetime.split("-")[0];
-    if (!memo[year]) memo[year] = [];
+    memo[year] = memo[year] ? memo[year] : [];
     memo[year].push(post);
     return memo;
   }, {});
