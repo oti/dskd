1f7bf900b077cd35dc7636cbf48ce4c5b1f4b85c
oti <otiext@gmail.com>
2023-03-16 16:02:50 +0900

    refactor: task/matters.mjs

--- task/matters.mjs
+++ task/matters.mjs
@@ -1,10 +1,11 @@
 import fs from "fs/promises";
 import fg from "fast-glob";
 import matter from "gray-matter";
+import { S_MD } from "./constants.mjs";
 
 export const matters = async () =>
   Promise.all(
-    (await fg("src/md/**/*.md")).map(async (filepath) =>
+    (await fg(S_MD)).map(async (filepath) =>
       matter(await fs.readFile(filepath, "utf8"))
     )
   );
