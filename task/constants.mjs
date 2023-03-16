55b4b9614a17bc7d1390f8faf6b65c3e0a2ffdc8
oti <otiext@gmail.com>
2023-03-16 16:02:41 +0900

    feat: task/constants.mjs

---
+++ task/constants.mjs
@@ -0,0 +1,5 @@
+export const D_ARCHIVE = "dist/archives/";
+export const D_HOME = "dist/";
+export const D_TAG = "dist/archives/tags/";
+export const D_YEAR = "dist/archives/years/";
+export const S_MD = "src/md/**/*.md";
