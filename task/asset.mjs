bcd12c64bc6f297ff1761b1a41d3bf85de02d16a
oti <otiext@gmail.com>
2023-05-06 17:49:34 +0900

    refactor: task/asset.mjs

--- task/asset.mjs
+++ task/asset.mjs
@@ -3,10 +3,19 @@ import fg from "fast-glob";
 import path from "path";
 import { S_ASSET } from "../dskd.config.mjs";
 
+const error = (error) => {
+  console.error(error);
+  throw error;
+};
+
 export const asset = async () =>
-  (await fg(S_ASSET)).map(async (source) => {
-    await fs.mkdir(path.parse(source).dir.replace("src", "dist"), {
-      recursive: true,
-    });
-    return await fs.copyFile(source, source.replace("src", "dist"));
+  (await fg(S_ASSET)).map(async (filepath) => {
+    await fs
+      .mkdir(path.parse(filepath).dir.replace("src", "dist"), {
+        recursive: true,
+      })
+      .catch(error)
+      .then(
+        async () => await fs.copyFile(filepath, filepath.replace("src", "dist"))
+      );
   });
