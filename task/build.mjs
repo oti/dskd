e5099bfa5b9b1eedd63c96e7449150fa3fff1310
oti <otiext@gmail.com>
2023-10-05 11:59:27 +0900

    feat: `npm run html` を追加した

--- task/build.mjs
+++ task/build.mjs
@@ -3,4 +3,6 @@ import { database } from "./database.mjs";
 import { html } from "./html.mjs";
 import { matters } from "./matters.mjs";
 
-await asset().then(matters).then(database).then(html);
+process.argv[2] == "--only-html"
+  ? await matters().then(database).then(html)
+  : await asset().then(matters).then(database).then(html);
