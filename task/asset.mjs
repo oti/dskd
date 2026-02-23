764e4c65cb9d147d79a5da8c12e516121596fb36
oti <otiext@gmail.com>
2026-02-23 22:52:18 +0900

    feat: add `Promise.all()` to `asset()`

--- task/asset.mjs
+++ task/asset.mjs
@@ -2,6 +2,8 @@ import fs from "node:fs/promises";
 import { S_ASSET } from "../src/config.mjs";
 
 export const asset = async () =>
-  (await Array.fromAsync(await fs.glob(S_ASSET))).map(
-    async (filepath) => await fs.cp(filepath, filepath.replace("src", "dist")),
+  Promise.all(
+    (await Array.fromAsync(await fs.glob(S_ASSET))).map(
+      async (filepath) => await fs.cp(filepath, filepath.replace("src", "dist")),
+    )
   );
